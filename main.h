6be77fddf86ab3a52e516d7aea2191867d9a1fab
c-kriss <benckriss@gmail.com>
2023-07-23 14:21:39 +0200

    Printf testing

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+#endif
